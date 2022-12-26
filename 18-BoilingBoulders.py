0103e37911515e5f119e9e10423a5543c39f8e0b
Windsor, Jeff <jwindsor@cj.com>
2022-12-26 11:51:31 -0800

    18 refactor

--- 18-BoilingBoulders.py
+++ 18-BoilingBoulders.py
@@ -1,15 +1,17 @@
-#https://adventofcode.com/2022/day/18
+#https:/entofcode.com/2022/day/18
 from collections import deque
 from functools import reduce
-# set points
+# set ponts
+
 # search for next to it
 
-def parse_inputs(filename):
-    return set([ tuple([int(c) for c in l.split(',')]) for l in open(filename,'r').readlines()])
+def parse_file(filename):
+    return set([ tuple([int(c) 
+                    for c in l.split(',')]) 
+                    for l in open(filename,'r').readlines()])
 
-
-examples = parse_inputs('examples/18')
-actuals = parse_inputs('inputs/18')
+examples = parse_file('examples/18')
+inputs   = parse_file('inputs/18')
 
 
 def neighbors(x, y, z): 
@@ -17,59 +19,43 @@ def neighbors(x, y, z):
             (x, y + 1, z),(x, y - 1, z),
             (x, y, z + 1),(x, y, z - 1)]
 
+def count_exposed_sides(points):
+    return sum([sum([(n not in points) 
+                for n in neighbors(*p)]) 
+                for p in points] )
 
-def count_sides_exposed_to_air(points):
-    return sum([ sum([(n not in points) for n in neighbors(*p)]) 
-                       for p in points] )
+def define_bounds(points):
+    mm = 1_000_000
+    minmax = lambda nx,mx,ny,my,nz,mz,x,y,z:(min(nx,x)-1, max(mx,x)+1, min(ny,y)-1, max(my,y)+1, min(nz,z)-1, max(mz,z)+1)
+    nx,xx,ny,xy,nz,xz = reduce(lambda acc,p: minmax(*acc,*p), points, (mm,-mm,mm,-mm,mm,-mm))
+    all_points = set([(x,y,z) for x in range(nx,xx + 1) for y in range(ny, xy + 1) for z in range(nz, xz + 1)])
+    is_inbounds = lambda x,y,z: nx <= x and x <= xx and ny <= y and y <= xy and nz <= z and z <= xz    
+    return all_points, is_inbounds
 
-def in_bounds(minx,maxx,miny,maxy,minz,maxz,x,y,z):
-    return minx <= x and x <= maxx and miny <= y and y <= maxy and minz <= z and z <= maxz
-
-def minmax(minx,maxx,miny,maxy,minz,maxz,x,y,z):
-    return (min(minx,x) - 1 , max(maxx,x) + 1, min(miny,y) - 1, max(maxy,y) + 1, min(minz,z) - 1, max(maxz,z) + 1)
-    
 def count_external_sides(points):
-    m = 10000000
-    minx,maxx,miny,maxy,minz,maxz = reduce(lambda acc,p: minmax(*acc,*p), points, (m,-m,m,-m,m,-m))
-        
-    visited = set()
+    all_points, is_inbounds = define_bounds(points)
     
-    d = deque()    #outside sphere point
-    d.append((0,0,0))
+    visited = set()
+    d = deque([(0,0,0)])    #outside sphere point
     while(d):
         p = d.popleft()
-        if p in visited: 
-            continue
+        if p in visited: continue
         
         visited.add(p)
-        if p in points: 
-            continue
+        if p in points: continue        
         
-        d.extend([(x,y,z) for x,y,z in neighbors(*p) if in_bounds(minx,maxx,miny,maxy,minz,maxz,x,y,z)])
+        d.extend(n for n in neighbors(*p) if is_inbounds(*n))
 
-    all_points = set([(x,y,z) for x in range(minx,maxx + 1) for y in range(miny, maxy + 1) for z in range(minz, maxz + 1)])
     filled = all_points - (visited - points)
-    return count_sides_exposed_to_air(filled)
+    return count_exposed_sides(filled)
 
 # answers #############################################################
-print('Part 1: ', count_sides_exposed_to_air(actuals))
+#print('Part 1: ', count_exposed_sides(inputs))
+#print('Part 2: ', count_external_sides(inputs))
 
 
 # tests ###############################################################
 def test_count_external_sides(): assert count_external_sides(examples) == 58
-def test_count_sides_exposed_to_air(): assert count_sides_exposed_to_air(examples) == 64
+def test_count_exposed_sides(): assert count_exposed_sides(examples) == 64
 
-def test_inputs(): assert examples == {
-    (2,2,2),
-    (1,2,2),
-    (3,2,2),
-    (2,1,2),
-    (2,3,2),
-    (2,2,1),
-    (2,2,3),
-    (2,2,4),
-    (2,2,6),
-    (1,2,5),
-    (3,2,5),
-    (2,1,5),
-    (2,3,5)}
+def test_inputs(): assert examples == {(2,2,2), (1,2,2), (3,2,2), (2,1,2), (2,3,2), (2,2,1), (2,2,3), (2,2,4), (2,2,6), (1,2,5), (3,2,5), (2,1,5), (2,3,5)}
