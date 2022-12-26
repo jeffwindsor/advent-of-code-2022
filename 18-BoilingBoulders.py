34d92843ecf60061689e21f0bb53e1cebfdb9f33
Jeff Windsor <jeff.windsor@gmail.com>
2022-12-26 14:37:27 -0800

    fixed looping visited

--- 18-BoilingBoulders.py
+++ 18-BoilingBoulders.py
@@ -1,18 +1,14 @@
 #https:/entofcode.com/2022/day/18
 from collections import deque
 from functools import reduce
-# set ponts
-
-# search for next to it
 
 def parse_file(filename):
     return set([ tuple([int(c) 
                     for c in l.split(',')]) 
                     for l in open(filename,'r').readlines()])
-
+max_width = 100
 examples = parse_file('examples/18')
 inputs   = parse_file('inputs/18')
-
 
 def neighbors(x, y, z): 
     return [(x + 1, y, z),(x - 1, y, z),
@@ -20,17 +16,21 @@ def neighbors(x, y, z):
             (x, y, z + 1),(x, y, z - 1)]
 
 def count_exposed_sides(points):
-    return sum([sum([(n not in points) 
-                for n in neighbors(*p)]) 
-                for p in points] )
+    empty_neighbors_per_point = (sum(n not in points for n in neighbors(*p)) for p in points)
+    return sum(empty_neighbors_per_point)
+
+def bound_ranges(points):
+    minmax = lambda nx,mx,ny,my,nz,mz,x,y,z: (min(nx,x), max(mx,x), min(ny,y), max(my,y), min(nz,z), max(mz,z))
+    nx,mx,ny,my,nz,mz = reduce(lambda acc,p: minmax(*acc,*p), points, (max_width,-max_width,max_width,-max_width,max_width,-max_width))
+    return (nx-1,mx+1,ny-1,my+1,nz-1,mz+1)
+
+def is_inbounds(nx,xx,ny,xy,nz,xz):
+    return lambda x,y,z: nx <= x and x <= xx and ny <= y and y <= xy and nz <= z and z <= xz            
 
 def define_bounds(points):
-    mm = 1_000_000
-    minmax = lambda nx,mx,ny,my,nz,mz,x,y,z:(min(nx,x)-1, max(mx,x)+1, min(ny,y)-1, max(my,y)+1, min(nz,z)-1, max(mz,z)+1)
-    nx,xx,ny,xy,nz,xz = reduce(lambda acc,p: minmax(*acc,*p), points, (mm,-mm,mm,-mm,mm,-mm))
-    all_points = set([(x,y,z) for x in range(nx,xx + 1) for y in range(ny, xy + 1) for z in range(nz, xz + 1)])
-    is_inbounds = lambda x,y,z: nx <= x and x <= xx and ny <= y and y <= xy and nz <= z and z <= xz    
-    return all_points, is_inbounds
+    nx,xx,ny,xy,nz,xz = bound_ranges(points)
+    all_points = set([(x,y,z) for x in range(nx,xx + 1) for y in range(ny, xy + 1) for z in range(nz, xz + 1)]) 
+    return all_points, is_inbounds(nx,xx,ny,xy,nz,xz)
 
 def count_external_sides(points):
     all_points, is_inbounds = define_bounds(points)
@@ -44,18 +44,18 @@ def count_external_sides(points):
         visited.add(p)
         if p in points: continue        
         
-        d.extend(n for n in neighbors(*p) if is_inbounds(*n))
+        d.extend(n for n in neighbors(*p) if is_inbounds(*n) and n not in visited)
 
     filled = all_points - (visited - points)
     return count_exposed_sides(filled)
+
+
+# tests ###############################################################
+def test_inputs(): assert examples == {(2,2,2), (1,2,2), (3,2,2), (2,1,2), (2,3,2), (2,2,1), (2,2,3), (2,2,4), (2,2,6), (1,2,5), (3,2,5), (2,1,5), (2,3,5)}
+def test_count_exposed_sides(): assert count_exposed_sides(examples) == 64
+def test_count_external_sides(): assert count_external_sides(examples) == 58
 
 # answers #############################################################
 print('Part 1: ', count_exposed_sides(inputs))
 print('Part 2: ', count_external_sides(inputs))
 
-
-# tests ###############################################################
-def test_count_external_sides(): assert count_external_sides(examples) == 58
-def test_count_exposed_sides(): assert count_exposed_sides(examples) == 64
-
-def test_inputs(): assert examples == {(2,2,2), (1,2,2), (3,2,2), (2,1,2), (2,3,2), (2,2,1), (2,2,3), (2,2,4), (2,2,6), (1,2,5), (3,2,5), (2,1,5), (2,3,5)}
