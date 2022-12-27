ff73a2849f5b7aec1a11aa660d96b05b5397af2d
Jeff Windsor <jeff.windsor@gmail.com>
2022-12-26 16:49:14 -0800

    Refactor

--- 18-BoilingBoulders.py
+++ 18-BoilingBoulders.py
@@ -6,7 +6,6 @@ def parse_file(filename):
     return set([ tuple([int(c) 
                     for c in l.split(',')]) 
                     for l in open(filename,'r').readlines()])
-max_width = 100
 examples = parse_file('examples/18')
 inputs   = parse_file('inputs/18')
 
@@ -19,33 +18,35 @@ def count_exposed_sides(points):
     empty_neighbors_per_point = (sum(n not in points for n in neighbors(*p)) for p in points)
     return sum(empty_neighbors_per_point)
 
-def bound_ranges(points):
+def bounds_around(points):
+    initial_minmax = (100,-100,100,-100,100,-100)
     minmax = lambda nx,mx,ny,my,nz,mz,x,y,z: (min(nx,x), max(mx,x), min(ny,y), max(my,y), min(nz,z), max(mz,z))
-    nx,mx,ny,my,nz,mz = reduce(lambda acc,p: minmax(*acc,*p), points, (max_width,-max_width,max_width,-max_width,max_width,-max_width))
+    nx,mx,ny,my,nz,mz = reduce(lambda acc,p: minmax(*acc,*p), points, initial_minmax)
     return (nx-1,mx+1,ny-1,my+1,nz-1,mz+1)
 
-def is_inbounds(nx,xx,ny,xy,nz,xz):
+def is_in(nx,xx,ny,xy,nz,xz):
     return lambda x,y,z: nx <= x and x <= xx and ny <= y and y <= xy and nz <= z and z <= xz            
 
-def define_bounds(points):
-    nx,xx,ny,xy,nz,xz = bound_ranges(points)
-    all_points = set([(x,y,z) for x in range(nx,xx + 1) for y in range(ny, xy + 1) for z in range(nz, xz + 1)]) 
-    return all_points, is_inbounds(nx,xx,ny,xy,nz,xz)
+def all_points_in(nx,xx,ny,xy,nz,xz):
+    return set([(x,y,z) for x in range(nx,xx + 1) for y in range(ny, xy + 1) for z in range(nz, xz + 1)]) 
 
-def count_external_sides(points):
-    all_points, is_inbounds = define_bounds(points)
-    visited = set()
+def count_external_sides(boulders):
+    bounds = bounds_around(boulders)
+    is_in_bounds = is_in(*bounds)
+    all_points_in_bounds = all_points_in(*bounds)
+
+    visited_points = set()
     d = deque([(0,0,0)])    #outside sphere point
     while(d):
         p = d.popleft()
-        if p in visited: continue
+        if p in visited_points: continue
         
-        visited.add(p)
-        if p in points: continue        
+        visited_points.add(p)
+        if p in boulders: continue        
         
-        d.extend(n for n in neighbors(*p) if is_inbounds(*n) and n not in visited)
+        d.extend(n for n in neighbors(*p) if is_in_bounds(*n) and n not in visited_points)
 
-    return count_exposed_sides(all_points - (visited - points))
+    return count_exposed_sides(all_points_in_bounds - (visited_points - boulders))
 
 
 # tests ###############################################################
