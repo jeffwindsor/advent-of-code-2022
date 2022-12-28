002912d699397b99ed3aecf587c35b83e6506209
Windsor, Jeff <jwindsor@cj.com>
2022-12-28 09:43:33 -0800

    20 parse

--- 20-GrovePositioningSystem.py
+++ 20-GrovePositioningSystem.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
 #https://adventofcode.com/2022/day/20
+
+def parse_file(filename):
+    return [int(i) for i in open(filename, 'r').read().split('\n')]
+
+# tests (pytest) ##############################################################
+def test_parse_file(): assert parse_file('examples/20') == [1, 2, -3, 3, -2, 0, 4]
